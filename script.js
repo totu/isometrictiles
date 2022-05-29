57ad0fa409f57a061b6ef01cbe7cff493ea22755
Topi Tuulensuu <topi@topituulensuu.com>
2022-05-29 21:09:47 +0300

    we have a mega tile

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ const w = 134;
 const startOffSet = w/2;
 
 const load = () => {
+    const matrix = [];
     for (let i=0; i<5; i++) {
         for (let j=0; j<5; j++) {
-            const tile = document.createElement("img");
-            tile.src = "tile.png";
-            tile.dataset.i = [i, 0.5];
-            tile.dataset.j = [-1 * j, 0.5];
-            tile.style.position = "absolute";
-            tile.style.top = i*h*0.5+j*h*0.25 + "px";
-            tile.style.left = j*w*0.5 - startOffSet + "px";
-            document.body.appendChild(tile);
-            tiles.push(tile);
+            matrix.push([i, j]);
         }
     }
-    tiles.forEach(function(item, index, arr) {
-        console.log(item, index, arr)
+    matrix.forEach(function(item, index, arr) {
+        const i = item[0];
+        const j = item[1];
+        const tile = document.createElement("img");
+        tile.src = "tile.png";
+        tile.style.top = i*h*0.25+j*h*0.25 + "px";
+        tile.style.left = j*w*0.5-i*w*0.5 + window.innerWidth/2 - startOffSet+ "px";
+        document.body.appendChild(tile);
     });
 };
 
